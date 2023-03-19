77e2c7f38ef2be3024c673162ace9c5432c2b363
DuMark Allman <dumark.allman@yahoo.com>
2023-03-19 14:44:58 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//multiply
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+var random = Math.floor(Math.random() *10) + 1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+// maybe my math sucks??? 
+//What 2 numbers gives you the remainder of 4?
+
+const arr = [1, 15, 14, 20];
+let max = 20;
+let minimum = -Infinity;
+for (let number of numbers){
+    if (number > number)
+    max = number
+}
+console.log(max);
